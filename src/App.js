ef62bad54f1524fa6f8677a25e2e01dcb015cb9b
kspayne93 <kyle@investavante.com>
2019-01-10 17:37:23 -0700

    added state with lifecycle methods

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  state = {
+    product: '',
+    cart: []
+  }
+
+
+  componentDidMount() {
+    axios.get(`/api/cart`).then(res => {
+      this.setState({
+        cart: res.data
+      });
+    });
+  }
+
+  handleChange(prop, val) {
+    this.setState({
+      [prop]: val //the brackets around prop make it so that it will set state with the property name, rather than a property called 'prop'. The brackets make the property name getting set to state dynamic.
+    })
+  };
+
+  addProduct() {
+    axios.post('/api/cart', {product: this.state.product }).then((res =>
+      this.setState({
+        cart: res.data,
+        product: '' //once the product has been added to the cart, the product input will be reset because we reset state back to an empty string
+      })
+    ))
+   } 
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input 
+        value={this.state.product}
+        onChange={ (e) => this.handleChange('product', e.target.value) } 
+        type="text"/>
+        <button onClick={ () => this.addProduct() } >Send</button>
+        <h2> {this.state.cart} </h2>
       </div>
     );
   }
